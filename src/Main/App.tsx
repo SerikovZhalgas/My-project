ec894ffdc50e9d535a6fa5ac6a54cc05eb46fa0a
SerikovZhalgas <zhalgasserikov@gmail.com>
2022-09-20 00:44:05 +0600

    Commit some changes...

--- src/Main/App.tsx
+++ src/Main/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import {Pages} from "./UI/Pages/Pages";
 import {Header} from "./UI/Header/Header";
 import {HashRouter} from "react-router-dom";
 
 export const App = () => {
   return (
-    <div className="App">
+    <div>
       <HashRouter>
         <Header/>
         <Pages/>
--- src/Main/UI/common/SuperCheckbox/SuperCheckbox.tsx
+++ src/Main/UI/common/SuperCheckbox/SuperCheckbox.tsx
@@ -1,7 +1,6 @@
 import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
 import s from './SuperCheckbox.module.css'
 
-// тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperCheckboxPropsType = DefaultInputPropsType & {
@@ -11,17 +10,16 @@ type SuperCheckboxPropsType = DefaultInputPropsType & {
 
 const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        type,
         onChange, onChangeChecked,
         className, spanClassName,
-        children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
-
-        ...restProps// все остальные пропсы попадут в объект restProps
+        children,
+        ...restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         onChange && onChange(e)
-        onChangeChecked && onChangeChecked(e.currentTarget.checked)// сделайте так чтоб работал onChange и onChangeChecked
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
@@ -32,11 +30,10 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
                 type={'checkbox'}
                 onChange={onChangeCallback}
                 className={finalInputClassName}
-
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
+                {...restProps}
             />
             {children && <span className={s.spanClassName}>{children}</span>}
-        </label> // благодаря label нажатие на спан передастся в инпут
+        </label>
     )
 }
 
