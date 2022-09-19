52f519ff3e370d27ea1cfdd5469a1a910b42a0cb
SerikovZhalgas <zhalgasserikov@gmail.com>
2022-09-20 00:49:37 +0600

    Commit Test page.

--- src/Main/UI/Pages/Components/Test.tsx
+++ src/Main/UI/Pages/Components/Test.tsx
@@ -1,5 +1,14 @@
+import SuperCheckbox from "../../common/SuperCheckbox/SuperCheckbox";
+import SuperButton from "../../common/SuperButton/SuperButton";
+import SuperInputText from "../../common/SuperInputText/SuperInputText";
+
 export const Test = () => {
+
     return <div>
-        Test
+        <SuperCheckbox/>
+        <SuperButton>
+            Button
+        </SuperButton>
+        <SuperInputText/>
     </div>
 }
